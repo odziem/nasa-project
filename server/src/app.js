da08c93051267181058057c363ef7d26481d38d6
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-03-12 11:53:13 -0500

    Add CORS

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 app.use(express.json());
 app.use(planetsRouter);
 
