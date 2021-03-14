a107688ded58e8247d81cb28aa9c1d4513a8ff46
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-03-14 14:45:05 -0400

    Build and deploy project

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -8,7 +9,13 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
