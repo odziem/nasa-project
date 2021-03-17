825c7d4f64ce1608147e9a9160ef23372582f2e6
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-03-17 10:06:56 -0400

    Serving with client side routing

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
 app.use(launchesRouter);
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
