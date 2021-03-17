6e88ceb7b076b423a485e363c5cfe5641ac6ed53
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-03-17 05:43:53 -0400

    Add morgan logging

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
