87dcd78cac38a4604fc25d927e8a8d7aa22e9d41
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-03-20 07:39:23 -0400

    Add new launch validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -10,7 +10,19 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
+  if (!launch.mission || !launch.rocket || !launch.launchDate
+    || !launch.destination) {
+      return res.status(400).json({
+        error: 'Missing required launch property',
+      });
+    }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: 'Invalid launch date',
+    });
+  }
 
   addNewLaunch(launch);
   return res.status(201).json(launch);
