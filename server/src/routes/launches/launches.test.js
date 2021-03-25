b293a5fb6d64196d944974b6ec27c3acbe0200d3
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-03-25 02:17:41 -0400

    Basic Jest test setup

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,15 @@
+describe('Test GET /launches', () => {
+  test('It should respond with 200 success', () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe('Test POST /launch', () => {
+  test('It should respond with 200 success', () => {
+
+  });
+
+  test('It should catch missing required properties', () => {});
+  test('It should catch invalid dates', () => {});
+});
