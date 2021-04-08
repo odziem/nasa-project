8931b01879df02ffe31309bb7253f447c93154b6
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-08 07:34:56 -0400

    Connect to Mongo cluster

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -6,9 +7,26 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+// Update below to match your own MongoDB connection string.
+const MONGO_URL = 'mongodb+srv://nasa-api:qX4ciaDgNwIROfPp@nasacluster.dtciu.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
   
   server.listen(PORT, () => {
