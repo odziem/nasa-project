23e2a33530c42315d1a7dd6b5e393da3676f8fb3
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-10 05:15:08 -0400

    Launches mongoose schema

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -8,7 +8,7 @@ const launch = {
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
   target: 'Kepler-442 b',
-  customer: ['ZTM', 'NASA'],
+  customers: ['ZTM', 'NASA'],
   upcoming: true,
   success: true,
 };
---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: [ String ],
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
