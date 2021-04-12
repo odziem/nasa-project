754ba371cfac8ca8097a9a9a4a8b7ff424798cb0
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-12 14:05:53 -0400

    Saving Launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,4 @@
-// const launches = require('./launches.mongo');
+const launchesDatabase = require('./launches.mongo');
 
 const launches = new Map();
 
@@ -15,7 +15,7 @@ const launch = {
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
   return launches.has(launchId);
@@ -23,6 +23,14 @@ function existsLaunchWithId(launchId) {
 
 function getAllLaunches() {
   return Array.from(launches.values());
+}
+
+async function saveLaunch(launch) {
+  await launchesDatabase.updateOne({
+    flightNumber: launch.flightNumber,
+  }, launch, {
+    upsert: true,
+  });
 }
 
 function addNewLaunch(launch) {
