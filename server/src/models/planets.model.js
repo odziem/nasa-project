36aa620bd98ecca0fdf9f2547eccca16158f2acb
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-12 13:10:04 -0400

    Exclude __v and _id

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -35,7 +35,9 @@ function loadPlanetsData() {
 }
 
 async function getAllPlanets() {
-  return await planets.find({});
+  return await planets.find({}, {
+    '_id': 0, '__v': 0,
+  });
 }
 
 async function savePlanet(planet) {
