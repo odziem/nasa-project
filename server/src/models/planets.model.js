8de3df3875483a32a5f17f766f122c07cf691e92
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-12 00:46:23 -0400

    Find and save planets all together

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -4,8 +4,6 @@ const parse = require('csv-parse');
 
 const planets = require('./planets.mongo');
 
-const habitablePlanets = [];
-
 function isHabitablePlanet(planet) {
   return planet['koi_disposition'] === 'CONFIRMED'
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
@@ -21,25 +19,37 @@ function loadPlanetsData() {
       }))
       .on('data', async (data) => {
         if (isHabitablePlanet(data)) {
-          // TODO: Replace below create with insert + update = upsert
-          // await planets.create({
-          //   keplerName: data.kepler_name,
-          // });
+          savePlanet(data);
         }
       })
       .on('error', (err) => {
         console.log(err);
         reject(err);
       })
-      .on('end', () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on('end', async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found!`);
         resolve();
       });
   });
 }
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find({});
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne({
+      keplerName: planet.kepler_name,
+    }, {
+      keplerName: planet.kepler_name,
+    }, {
+      upsert: true,
+    });
+  } catch(err) {
+    console.error(`Could not save planet ${err}`);
+  }
 }
 
 module.exports = {
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,7 +1,7 @@
 const { getAllPlanets } = require('../../models/planets.model');
 
-function httpGetAllPlanets(req, res) {
-  return res.status(200).json(getAllPlanets());
+async function httpGetAllPlanets(req, res) {
+  return res.status(200).json(await getAllPlanets());
 }
 
 module.exports = {
