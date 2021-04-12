8985a2bc4f700e5dc1ae51d97813de7ed30dc75e
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-12 19:36:56 -0400

    Verify target planet exists

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,5 @@
 const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
 const launches = new Map();
 
@@ -27,6 +28,14 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet found');
+  }
+
   await launchesDatabase.updateOne({
     flightNumber: launch.flightNumber,
   }, launch, {
