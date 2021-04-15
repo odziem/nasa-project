cce49d722ca83564e2078d4b797593b1743d268e
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-15 14:36:03 -0400

    Use findOneAndUpdate

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -48,7 +48,7 @@ async function saveLaunch(launch) {
     throw new Error('No matching planet found');
   }
 
-  await launchesDatabase.updateOne({
+  await launchesDatabase.findOneAndUpdate({
     flightNumber: launch.flightNumber,
   }, launch, {
     upsert: true,
