bb7a503654d1d196060e264816c1c244ec33e31e
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-21 02:44:15 -0400

    Save SpaceX launches to database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -43,6 +43,11 @@ async function populateLaunches() {
     }
   });
 
+  if (response.status !== 200) {
+    console.log('Problem downloading launch data');
+    throw new Error('Launch data download failed');
+  }
+
   const launchDocs = response.data.docs;
   for (const launchDoc of launchDocs) {
     const payloads = launchDoc['payloads'];
@@ -62,7 +67,7 @@ async function populateLaunches() {
 
     console.log(`${launch.flightNumber} ${launch.mission}`);
 
-    //TODO: populate launches collection...
+    await saveLaunch(launch);
   }
 }
 
@@ -107,14 +112,6 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  });
-
-  if (!planet) {
-    throw new Error('No matching planet found');
-  }
-
   await launchesDatabase.findOneAndUpdate({
     flightNumber: launch.flightNumber,
   }, launch, {
@@ -123,6 +120,14 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet found');
+  }
+
   const newFlightNumber = await getLatestFlightNumber() + 1;
 
   const newLaunch = Object.assign(launch, {
--- server/src/models/launches.mongo.js
+++ server/src/models/launches.mongo.js
@@ -19,7 +19,6 @@ const launchesSchema = new mongoose.Schema({
   },
   target: {
     type: String,
-    required: true,
   },
   customers: [ String ],
   upcoming: {
