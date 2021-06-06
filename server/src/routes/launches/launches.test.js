684b58e2bdf53866299994e0a6df4c0477414a53
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-06-06 08:11:51 -0400

    Load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -4,10 +4,14 @@ const {
   mongoConnect,
   mongoDisconnect,
 } = require('../../services/mongo');
+const {
+  loadPlanetsData,
+} = require('../../models/planets.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
