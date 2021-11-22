647fda4c30a6c14a453ec84ddab8d38e6b1624fa
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-11-21 19:45:03 -0500

    Populate MONGO_URL whenever connecting to DB

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
 // Update below to match your own MongoDB connection string.
 const MONGO_URL = process.env.MONGO_URL;
-
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
